345f640be186b61f542909259f62e92bb3fd4ae8
Kevhagi <94234971+Kevhagi@users.noreply.github.com>
2022-02-16 14:21:53 +0700

    Preparation server DONE

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 const express = require('express')
 const router = require('./src/routes')
+const cors = require('cors')
 
 const app = express()
 
 const port = 5000
 
 app.use(express.json())
-
 app.use('/api/v1/', router)
+app.use(cors())
 
 app.use('/uploads', express.static('uploads'))
 
